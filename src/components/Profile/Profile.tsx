7ff98d1912e87ea1ca88313140d127d3dbd281ce
Alexandra Kondratenko <frida21am@mail.ru>
2025-01-03 15:56:18 +0300

    Added components Button and Profile

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <img
+      src="https://s3.stroi-news.ru/img/kartinki-zimi-na-telefon-19.jpg"
+      alt="Профиль пользователя"
+    />
+  );
+};
+
+export default Profile;
